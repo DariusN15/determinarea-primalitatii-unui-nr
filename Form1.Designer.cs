37ca1f3660aacd167c95f47fa38042316cc783b6
DariusN15 <darius.nastase@cncn.ro>
2023-03-01 21:27:49 +0200

    commit data 3/1/2023

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace determinarea_primalitatii_unui_nr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BUTON = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BUTON
+            // 
+            this.BUTON.Location = new System.Drawing.Point(288, 204);
+            this.BUTON.Name = "BUTON";
+            this.BUTON.Size = new System.Drawing.Size(239, 115);
+            this.BUTON.TabIndex = 0;
+            this.BUTON.Text = "button1";
+            this.BUTON.UseVisualStyleBackColor = true;
+            this.BUTON.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(338, 105);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(135, 47);
+            this.TextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.BUTON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BUTON;
+        private System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
